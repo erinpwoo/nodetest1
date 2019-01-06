1c9b89e15933e41d391e3747a86b26add7f04413
Erin Woo <erinpwoo@umail.ucsb.edu>
2019-01-05 21:03:35 -0800

    working userlist

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-//New Code
+// New Code
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost27017/nodetest1'); //27017 default port for database
+var db = monk('localhost:27017/nodetest1');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express(); //This one's important. It instantiates Express and assigns our app variable to it
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,15 +24,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Make our db accessible to our router
-app.use(function(req, res, next) {
-  req.db = db;
-  next();
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
 });
 
-//app.use statements provide functionality that rest of app can use (middleware for Express)
-//comes before route statements so route can use them
-app.use('/', indexRouter); //different routes-- for now we'll ignore usersRouter
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,14 @@ router.get('/helloworld', function(req, res) {
   res.render('helloworld', {title: 'Hello, World!'});
 });
 
-/*GET Userlist page. */
-router.get('/userlist', function(req, res){
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
   var db = req.db;
   var collection = db.get('usercollection');
-  collection.find({}, {}, function(e, docs) {
-    res.render('userlist', {
-      "userlist" : docs
-    });
+  collection.find({},{},function(e,docs){
+      res.render('userlist', {
+          "userlist" : docs
+      });
   });
 });
 
