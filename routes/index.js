2a7512503b97d6a9a6a267aecf87f1bb4510af9d
Erin Woo <erinpwoo@umail.ucsb.edu>
2019-01-05 21:34:38 -0800

    added adduser function, finished tutorial

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,37 @@ router.get('/userlist', function(req, res) {
   });
 });
 
+/*GET New User page. */
+router.get('/newuser', function(req, res) {
+  res.render('newuser', { title: 'Add New User' });
+});
+
+router.post('/adduser', function(req, res){
+  //set up local db variable
+  var db = req.db;
+
+  //get our form values. these rely on the "name" attributes from newuser.jade
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  //set collection
+  var collection = db.get('usercollection');
+
+  //submit to the db
+  collection.insert({
+    "username" : userName,
+    "useremail" : userEmail
+    } , function (err, doc) {
+      if (err) {
+        //fail case
+        res.send("There was a problem adding information to the database.");
+      } else {
+        //forward to success page
+        res.redirect("userlist");
+      }
+    });
+  });
+
 module.exports = router;
 
 //Changes to Jade templates do not require a server restart (npm start), but changes to a js file do (i.e. app.js)
